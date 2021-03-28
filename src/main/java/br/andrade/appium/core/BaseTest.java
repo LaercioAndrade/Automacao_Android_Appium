8b05461c1d671eb91e857f35ea378a23acdda960
LaercioAndrade <andradelaercioo@hotmail.com>
2021-03-27 22:40:09 -0300

    configurando o app para reiniciar após cada teste

--- src/main/java/br/andrade/appium/core/BaseTest.java
+++ src/main/java/br/andrade/appium/core/BaseTest.java
@@ -1,12 +1,18 @@
 package br.andrade.appium.core;
 
 import org.junit.After;
+import org.junit.AfterClass;
 
 public class BaseTest {
 
+	@AfterClass
+	public static void finalizarClasse() {
+		DriverFactory.KillDriver();
+	}
+	
 	
 	@After
 	public void tearDown() {
-		DriverFactory.KillDriver();
+		DriverFactory.getDriver().resetApp();
 	}
 }
