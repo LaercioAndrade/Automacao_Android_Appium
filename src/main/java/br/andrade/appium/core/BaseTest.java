08e0ee1c81b5ebeaaad3216ad293202f55e63b96
LaercioAndrade <andradelaercioo@hotmail.com>
2021-03-27 22:57:33 -0300

    configurando screenshots

--- src/main/java/br/andrade/appium/core/BaseTest.java
+++ src/main/java/br/andrade/appium/core/BaseTest.java
@@ -1,9 +1,21 @@
 package br.andrade.appium.core;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class BaseTest {
+	
+	@Rule
+	public TestName testName = new TestName();
+
 
 	@AfterClass
 	public static void finalizarClasse() {
@@ -13,6 +25,16 @@ public class BaseTest {
 	
 	@After
 	public void tearDown() {
+		gerarScreenShot();
 		DriverFactory.getDriver().resetApp();
+	}
+	
+	public void gerarScreenShot() {
+		File imagem = ((TakesScreenshot) DriverFactory.getDriver()).getScreenshotAs(OutputType.FILE);
+		try {
+			FileUtils.copyFile(imagem, new File("target/screenshots/"+testName.getMethodName()+".png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
